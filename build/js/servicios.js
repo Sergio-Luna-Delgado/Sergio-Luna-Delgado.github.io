291d7a059acde236d1836acf9e132a91f6aca778
SergioLunaDelgado <sergioluna13102001@gmail.com>
2023-03-23 13:49:01 -0600

    Nuevos Servicios

---
+++ build/js/servicios.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){const e=window.scrollY,o=document.querySelector(".header");o&&(e>1?o.classList.add("header-mover"):o.classList.remove("header-mover"))};
+//# sourceMappingURL=servicios.js.map
